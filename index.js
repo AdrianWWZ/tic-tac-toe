d10a61dff24606a48819d7340cd7796b20638de0
Adrian Wong <adrianwongapple@gmail.com>
2024-04-01 20:29:40 +0800

    Finished the css to display the game and the js to reset the board

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let gameBoard = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+let gameCount = 0;
 gameBoard[0] = "O";
 gameBoard[1] = "O";
 gameBoard[2] = "X";
@@ -10,10 +11,17 @@ gameBoard[5] = "O";
 gameBoard[6] = "O";
 gameBoard[7] = "O";
 gameBoard[8] = "X";
-console.log(gameBoard);
 
 const resetBoard = () => {
   gameBoard = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+
+  inputBoxes.forEach((inputBox) => {
+    const input = inputBox.querySelector(".input");
+    input.classList.remove("disabled");
+    if (inputBox.children[1]) inputBox.removeChild(inputBox.children[1]);
+  });
+
+  gameCount = 0;
 };
 
 const winningPatterns = [
@@ -51,6 +59,25 @@ console.log(gameBoard);
 const inputBoxes = document.querySelectorAll(".input-box");
 inputBoxes.forEach((inputBox) => {
   inputBox.addEventListener("change", () => {
-    inputBox.innerText = "disabled";
+    if (gameCount % 2 === 0) {
+      const div = document.createElement("div");
+      div.classList.add("XBox");
+      div.innerText = "X";
+      inputBox.appendChild(div);
+    } else {
+      const div = document.createElement("div");
+      div.classList.add("OBox");
+      div.innerText = "O";
+      inputBox.appendChild(div);
+    }
+    gameCount++;
+
+    const input = inputBox.querySelector(".input");
+    input.classList.add("disabled");
   });
+});
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+  resetBoard();
 });
