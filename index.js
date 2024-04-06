c356a7d08d4cadb830580ceb2639b8ff5c0e9775
Adrian Wong <adrianwongapple@gmail.com>
2024-04-06 17:13:46 +0800

    Finished the logic for the game where the board resets when one wins or ties

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 let gameBoard = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
 let gameCount = 0;
-gameBoard[0] = "O";
-gameBoard[1] = "O";
-gameBoard[2] = "X";
-
-gameBoard[3] = "X";
-gameBoard[4] = "X";
-gameBoard[5] = "O";
-
-gameBoard[6] = "O";
-gameBoard[7] = "O";
-gameBoard[8] = "X";
 
 const resetBoard = () => {
   gameBoard = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
@@ -24,56 +13,65 @@ const resetBoard = () => {
   gameCount = 0;
 };
 
-const winningPatterns = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
+const checkGameBoard = () => {
+  const winningPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
-winningPatterns.forEach((pattern) => {
-  const a = pattern[0];
-  const b = pattern[1];
-  const c = pattern[2];
-  if (
-    gameBoard[a] !== " " &&
-    gameBoard[a] === gameBoard[b] &&
-    gameBoard[b] === gameBoard[c]
-  ) {
+  winningPatterns.forEach((pattern) => {
+    const a = pattern[0];
+    const b = pattern[1];
+    const c = pattern[2];
+    if (
+      gameBoard[a] !== " " &&
+      gameBoard[a] === gameBoard[b] &&
+      gameBoard[b] === gameBoard[c]
+    ) {
+      console.log(`${gameBoard[a]} won!`);
+      resetBoard();
+    }
+  });
+
+  let isFull = true;
+  gameBoard.forEach((piece) => {
+    if (piece === " ") isFull = false;
+  });
+  if (isFull) {
+    console.log("tie");
     resetBoard();
   }
-});
-
-let isFull = true;
-gameBoard.forEach((piece) => {
-  if (piece === " ") isFull = false;
-});
-if (isFull) console.log("tie");
-
-console.log(gameBoard);
+};
 
 const inputBoxes = document.querySelectorAll(".input-box");
 inputBoxes.forEach((inputBox) => {
   inputBox.addEventListener("change", () => {
+    const input = inputBox.querySelector(".input");
+
     if (gameCount % 2 === 0) {
       const div = document.createElement("div");
       div.classList.add("XBox");
       div.innerText = "X";
       inputBox.appendChild(div);
+      gameBoard[input.value - 1] = "X";
     } else {
       const div = document.createElement("div");
       div.classList.add("OBox");
       div.innerText = "O";
       inputBox.appendChild(div);
+      gameBoard[input.value - 1] = "O";
     }
+    console.log(gameBoard);
     gameCount++;
 
-    const input = inputBox.querySelector(".input");
     input.classList.add("disabled");
+    checkGameBoard();
   });
 });
 
